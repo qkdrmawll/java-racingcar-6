712f9f5914b62db9eb3cf3f4687ba1a68cd18726
qkdrmawll <eunji8106@naver.com>
2023-11-09 00:04:44 +0900

    refactor : 출력 방식 변경

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,12 +1,19 @@
 package racingcar.view;
 
+import racingcar.model.Player;
+
+import java.util.List;
+
 public class OutputView {
 
     public void printResultGuideComment() {
         System.out.println("실행 결과");
     }
-    public void printExecutionResult(String result) {
-        System.out.println(result);
+    public void printExecutionResult(List<Player> players) {
+        for (Player player : players) {
+            System.out.println(player.toString());
+        }
+        System.out.println();
     }
     public void printWinner(String winner) {
         System.out.println("최종 우승자 : "+winner);
