1b807abadecfd936958ac9a33261e1fb41a6639c
qkdrmawll <eunji8106@naver.com>
2023-11-09 00:09:20 +0900

    feat : 프로그램 실행 시작점 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,13 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+
 public class Application {
+    static RacingController racingController = new RacingController();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        racingController.racingSet();
+        racingController.racingStart();
+        racingController.racingEnd();
     }
 }
